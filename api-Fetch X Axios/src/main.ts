9ce024d1fc75df964fe0799a0c0c3999c61d2a4c
VinicinBraga <vinicius@bragamatta.com.br>
2022-08-24 16:22:50 -0300

    Fetch x Axios

---
+++ api-Fetch X Axios/src/main.ts
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+//URLs
+//Frases: http://api.adviceslip.com/advice
+//Rick and Morty: http://rickandmortyapi.com/api/character/73
+//Traduçoes: https://api.funtranslations.com/translate
+
+
+//fetch(Frases)
+ const buscarFrasesComFetch = async() => {
+  const response = await fetch('http://api.adviceslip.com/advice')
+   const data = await response.json()
+   console.log(`Com o fetch -> ${data.slip.advice}`)
+} 
+
+buscarFrasesComFetch()
+
+//axios(Frases)
+const buscarFrasesComAxios = async () => {
+  const response = await axios.get('http://api.adviceslip.com/advice')
+  console.log(`Com o Axios -> ${response.data.slip.advice}`)
+}
+buscarFrasesComAxios()
+
+//---------------------------------------
+
+//fetch(Traduçoes)
+const buscatTraducoesComFetch = async () => {
+  const body = { text: 'Hello my friend' }
+  const response = await fetch('https://api.funtranslations.com/translate/yoda', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {'content-type': 'application/json'}
+  })
+  const data = await response.json()
+  console.log(data)
+}
+buscatTraducoesComFetch()
+
+//axios(Traduçoes)
+const buscatTraducoesComAxios = async () => {
+  const body = { text: 'Hello my friend' }
+  const response = await axios.post('https://api.funtranslations.com/translate/yoda')
+  console.log(response.data)
+ 
+}
+buscatTraducoesComAxios()
