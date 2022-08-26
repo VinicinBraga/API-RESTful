f463c273728317020011c62618c76e9d60b605bc
VinicinBraga <vinicius@bragamatta.com.br>
2022-08-26 15:04:23 -0300

    Post useForm estruture

--- api-app/src/pages/Post/post.js
+++ api-app/src/pages/Post/post.js
@@ -1,17 +1,63 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import Header from "../../components/Header/Header";
+import "./post.css";
 
 function Post() {
   const {
     register,
     handleSubmit,
-    formState: { erros },
+    formState: { errors },
   } = useForm();
+
+  const addPost = (values) => {
+    console.log(values);
+  };
 
   return (
     <div>
       <Header />
+
+      <main>
+        <div className="card-post">
+          <h1>Create Post</h1>
+          <div className="line-post"></div>
+
+          <div className="card-body-post">
+            <form onSubmit={handleSubmit(addPost)}>
+              <div className="fields">
+                <label>Title</label>
+                <input type="text" name="title" {...register("title")} />
+                <p className="error-message"></p>
+              </div>
+
+              <div className="fields">
+                <label>Description</label>
+                <input
+                  type="text"
+                  name="description"
+                  {...register("description")}
+                />
+                <p className="error-message"></p>
+              </div>
+
+              <div className="fields">
+                <label>Content</label>
+                <textarea
+                  type="text"
+                  name="content"
+                  {...register("content")}
+                ></textarea>
+                <p className="error-message"></p>
+              </div>
+
+              <div className="btn-post">
+                <button type="submit">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
