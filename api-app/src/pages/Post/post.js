46d80ea87830ad54aed0da72f15c21ce78a1722e
VinicinBraga <vinicius@bragamatta.com.br>
2022-08-26 15:56:39 -0300

    Form Validation

--- api-app/src/pages/Post/post.js
+++ api-app/src/pages/Post/post.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import Header from "../../components/Header/Header";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import "./post.css";
+
+const validation = yup.object().shape({
+  title: yup
+    .string()
+    .required("*Required Field")
+    .max(15, "*Maximum 15 characters"),
+  description: yup
+    .string()
+    .required("*Required Field")
+    .max(150, "*Maximum 150 characters"),
+  content: yup
+    .string()
+    .required("*Required Field")
+    .max(500, "*Maximum 500 characters"),
+});
 
 function Post() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(validation),
+  });
 
   const addPost = (values) => {
     console.log(values);
@@ -28,7 +47,7 @@ function Post() {
               <div className="fields">
                 <label>Title</label>
                 <input type="text" name="title" {...register("title")} />
-                <p className="error-message"></p>
+                <p className="error-message">{errors.title?.message}</p>
               </div>
 
               <div className="fields">
@@ -38,7 +57,7 @@ function Post() {
                   name="description"
                   {...register("description")}
                 />
-                <p className="error-message"></p>
+                <p className="error-message">{errors.description?.message}</p>
               </div>
 
               <div className="fields">
@@ -48,7 +67,7 @@ function Post() {
                   name="content"
                   {...register("content")}
                 ></textarea>
-                <p className="error-message"></p>
+                <p className="error-message">{errors.content?.message}</p>
               </div>
 
               <div className="btn-post">
