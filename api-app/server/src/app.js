dbb145ee4471d2ada6d9da3883d2d40b842b1758
VinicinBraga <vinicius@bragamatta.com.br>
2022-08-30 17:03:45 -0300

    Back-end structure teste

--- api-app/server/src/app.js
+++ api-app/server/src/app.js
@@ -6,8 +6,9 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/create", (req, res) => {
   const title = req.body.title;
-
-  res.send(`Title: ${title}`);
+  const name = req.body.name;
+  console.log(`Title: ${title} and ${name}`);
+  res.send(`Title: ${title} and ${name}`);
 });
 
 app.listen(3001, () => console.log(`Example app listening on port 3001!`));
