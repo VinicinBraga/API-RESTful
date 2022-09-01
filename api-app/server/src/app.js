2c9e08e975d95e340624b4ea7662642f34004414
VinicinBraga <vinicius@bragamatta.com.br>
2022-09-01 15:31:54 -0300

    Praticing Routes

--- api-app/server/src/app.js
+++ api-app/server/src/app.js
@@ -6,15 +6,35 @@ const PostModel = require("./models/Posts");
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/create", (req, res) => {
-  const title = req.body.title;
-  res.send(`Title: ${title}`);
+app.post("/create_post", async (req, res) => {
+  try {
+    const { title, description, content } = req.body;
+    const post = await PostModel.create({ title, description, content });
+    res.send(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.post("/create_post", async (req, res) => {
-  const { title, description, content } = req.body;
-  const post = await PostModel.create({ title, description, content });
-  res.send(post);
+app.get("/list_posts", async (req, res) => {
+  try {
+    const posts = await PostModel.find({});
+    console.log(posts);
+    res.send({ posts });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get("/show_posts/:post_id", async (req, res) => {
+  try {
+    const postId = req.params.post_id;
+
+    const post = await PostModel.findById(postId);
+    res.send(post);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.listen(3001, () => console.log(`Example app listening on port 3001!`));
