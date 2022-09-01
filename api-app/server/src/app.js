da5e8ee4036e72afbc1939438c4ece51485f33e3
VinicinBraga <vinicius@bragamatta.com.br>
2022-09-01 12:19:52 -0300

    connection with mongodb

--- api-app/server/src/app.js
+++ api-app/server/src/app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const PostModel = require("./models/Posts");
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/create", (req, res) => {
   const title = req.body.title;
-  const name = req.body.name;
-  console.log(`Title: ${title} and ${name}`);
-  res.send(`Title: ${title} and ${name}`);
+  res.send(`Title: ${title}`);
+});
+
+app.post("/create_post", async (req, res) => {
+  const { title, description, content } = req.body;
+  const post = await PostModel.create({ title, description, content });
+  res.send(post);
 });
 
 app.listen(3001, () => console.log(`Example app listening on port 3001!`));
--- api-app/server/src/db/db.js
+++ api-app/server/src/db/db.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose
-  .connect("mongodb://localhost/adc-api", {
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("Mongo connected");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongoose.connect(
+  `mongodb+srv://vinicin:${process.env.PASSWORD}@api-clister.d9kc9xi.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.log(error);
+});
+db.once("open", () => console.log("Connected to the database"));
 
 module.exports = mongoose;
