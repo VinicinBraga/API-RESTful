69aa08df2c205cb43107c4fc4ef92c2556c82c16
VinicinBraga <vinicius@bragamatta.com.br>
2022-09-02 11:09:11 -0300

    Updating posts

--- api-app/server/src/app.js
+++ api-app/server/src/app.js
@@ -10,7 +10,7 @@ app.post("/create_post", async (req, res) => {
   try {
     const { title, description, content } = req.body;
     const post = await PostModel.create({ title, description, content });
-    res.send(post);
+    res.send({ post });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -31,7 +31,27 @@ app.get("/show_posts/:post_id", async (req, res) => {
     const postId = req.params.post_id;
 
     const post = await PostModel.findById(postId);
-    res.send(post);
+    res.send({ post });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.patch("/update_post/:post_id", async (req, res) => {
+  try {
+    const postId = req.params.post_id;
+    const { title, description, content } = req.body;
+    const post = await PostModel.findByIdAndUpdate(
+      postId,
+      {
+        title,
+        description,
+        content,
+      },
+      { new: true }
+    );
+
+    res.send({ post });
   } catch (error) {
     res.status(400).send(error);
   }
